cfb3c3fbc6255a7bd1ffb2e06b663196de6b4735
Fernando Ania <feroli@Fernandos-MacBook-Pro.local>
2017-11-07 11:37:20 +0000

    UPDATED: started classes page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  private fernandoFacebookUrl = 'https://www.facebook.com/Fer0li';
+  private danielFacebookUrl = 'https://www.facebook.com/daniel.chong.908';
+  private jasonFacebookUrl = 'https://www.facebook.com/jasondjjay.bachata.7';
+
   constructor() { }
+
+  goToFernandoFacebook() {
+    window.open(this.fernandoFacebookUrl, '_blank');
+  };
+
+  goToDanielFacebook() {
+    window.open(this.danielFacebookUrl, '_blank')
+  };
+
+  goToJasonFacebook() {
+    window.open(this.jasonFacebookUrl, '_blank')
+  }
 
   ngOnInit() {
   }
