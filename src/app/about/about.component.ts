4f299e6bc65fda3d4d6893ace0f2180a27ee0b92
Fernando Ania <feroli@Fernandos-MacBook-Pro.local>
2017-11-17 16:07:10 +0000

    added a find us button on about page and made all find us button disappear on desktop screens

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -10,6 +10,8 @@ export class AboutComponent implements OnInit {
   private fernandoFacebookUrl = 'https://www.facebook.com/Fer0li';
   private danielFacebookUrl = 'https://www.facebook.com/daniel.chong.908';
   private jasonFacebookUrl = 'https://www.facebook.com/jasondjjay.bachata.7';
+
+  visionText: string;
 
   constructor() { }
 
@@ -25,7 +27,16 @@ export class AboutComponent implements OnInit {
     window.open(this.jasonFacebookUrl, '_blank')
   }
 
+  findUs() {
+    $('html, body').animate({
+      scrollTop: $('.vision_container').offset().top
+    })
+  }
+
   ngOnInit() {
+    this.visionText = `
+    The BachaDIFF team focus will be for Bachata Social Dancing and we aim to highlight the music, culture and flow of the dance and most importantly to produce high quality dancers through quality teaching and attention to the basics and fundamentals focusing on concepts of movement, in a super FUN learning environment.
+    `
   }
 
 }
